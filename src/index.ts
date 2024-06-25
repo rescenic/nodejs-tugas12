f6cd992aedd3746307dfb217fd44ebd0bd734957
Muhammad Ridwan Hakim <adminrescen@gmail.com>
2024-06-25 12:38:21 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,19 @@ app.post("/login", (req: Request, res: Response) => {
   res.send("User logged in");
 });
 
+// Endpoint untuk menambah produk
+app.post('/products', (req: Request, res: Response) => {
+    const { name, price } = req.body;
+    // Logika untuk menambah produk
+    res.send('Product added');
+});
+
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+    // Logika untuk mendapatkan semua produk
+    res.send('List of products');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
